2da68c7dfeb53d754d492b10ee573bc35779f5ba
now123 <40191133+now123@users.noreply.github.com>
2018-06-29 01:19:31 +0530

    Update twe2.py

--- twe2.py
+++ twe2.py
@@ -1,6 +1,8 @@
 #QUE 1-
 print("An access token is an object that describes the security context of a process or thread. The information in a token includes the identity and privileges of the user account associated with the process or thread.")
-print ("access_token=1002830618831478785-KGYSD9LlrkiL4qSJbIortGaPM2JfYa")
+import tweepy
+access_token=1002830618831478785-KGYSD9LlrkiL4qSJbIortGaPM2JfYa
+print (access_token)
 
 
 #Que 2
