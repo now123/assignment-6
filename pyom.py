f7a2149e3dc11e2a0c56422a37863c6b38eb87e8
now123 <40191133+now123@users.noreply.github.com>
2018-07-12 21:05:42 +0530

    Add files via upload

--- pyom.py
+++ pyom.py
@@ -1,24 +1,79 @@
 from tkinter import*
+import tkinter
+import os
+from tkinter import *
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+
 root=Tk("Text Editor")
 root.title("Text Editor")
 text=Text(root)
-text.grid()
+# To add scrollbar
 
+scrollbar = Scrollbar(root)
+scrollbar.pack( side = RIGHT, fill = Y )
 
 def donothing():
     file = Toplevel(root)
     button = Button(file, text="abhi kuch nhi")
     button.pack()
 
+####
+def about():
+    showinfo("Notepad", "welcome to my world")
+
+
+def opn():
+    text.delete(1.0, END)
+
+    file = open(askopenfilename(), 'r')
+
+    if file != '':
+
+        txt = file.read()
+
+        text.insert(INSERT, txt)
+
+    else:
+
+        pass
+
+
+def save():
+    filename = asksaveasfilename()
+
+    if filename:
+        alltext =text.get(1.0, END)
+
+        open(filename, 'w').write(alltext)
+
+
+def copy():
+    text.clipboard_clear()
+
+    text.clipboard_append(text.selection_get())
+
+
+def paste():
+    try:
+
+        teext = text.selection_get(selection='CLIPBOARD')
+
+        text.insert(INSERT, teext)
+
+    except:
+
+        tkMessageBox.showerror("Error", "can't be done!")
+
+
 
 menubar = Menu(root)
 #adding the menus <1> file
 filemenu = Menu(menubar, tearoff=0)
-menubar.add_cascade(label="File", menu=filemenu)
+menubar.add_cascade(label="File",font="ariel 20", menu=filemenu)
 filemenu.add_command(label="New   ctrl+n", command=donothing)
-filemenu.add_command(label="Open  ctrl+o", command=donothing)
-filemenu.add_command(label="Save  ctrl+s", command=donothing)
-filemenu.add_command(label="Save as", command=donothing)
+filemenu.add_command(label="Open  ctrl+o", command=opn)
+filemenu.add_command(label="Save  ctrl+s", command=save)
 filemenu.add_command(label="Close", command=donothing)
 
 filemenu.add_separator()
@@ -29,8 +84,8 @@ filemenu.add_command(label="Exit", command=root.quit)
 editmenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Edit", menu=editmenu)
 editmenu.add_command(label="cut  ctrl+x", command=donothing)
-editmenu.add_command(label="copy ctrl+c", command=donothing)
-editmenu.add_command(label="paste ctrl+v", command=donothing)
+editmenu.add_command(label="copy ctrl+c", command=copy)
+editmenu.add_command(label="paste ctrl+v", command=paste)
 editmenu.add_command(label="delete", command=donothing)
 
 #adding menu<3> view
@@ -38,12 +93,10 @@ viewmenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="View", menu=viewmenu)
 viewmenu.add_command(label="status", command=donothing)
 viewmenu.add_command(label="recent file", command=donothing)
-viewmenu.add_command(label="toolbar", command=donothing)
 
 #adding menu<4> about
 viewmenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="About", menu=viewmenu)
-viewmenu.add_command(label="info", command=donothing)
-viewmenu.add_command(label="help", command=donothing)
+viewmenu.add_command(label="info", command=about)
 root.config(menu=menubar)
 root.mainloop()
